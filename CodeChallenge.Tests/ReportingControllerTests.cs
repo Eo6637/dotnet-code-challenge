3e8ece6bda53e58327880911c7284d15e6b66587
Eric Owens <ericowens@comcast.net>
2024-12-22 16:00:00 -0500

    Added ReportingStructureType
    ReportingStructure type with new controller and tests. Needs far more unit testing to verify the functionality of the controller and algorithm for structure information calculation.

---
+++ CodeChallenge.Tests/ReportingControllerTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using CodeChallenge.Models;
+using CodeCodeChallenge.Tests.Integration.Extensions;
+using CodeCodeChallenge.Tests.Integration.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeChallenge.Tests.Integration
+{
+    [TestClass]
+    public class ReportingControllerTests
+    {
+        private static HttpClient _httpClient;
+        private static TestServer _testServer;
+
+        [ClassInitialize]
+        // Attribute ClassInitialize requires this signature
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
+        public static void InitializeClass(TestContext context)
+        {
+            _testServer = new TestServer();
+            _httpClient = _testServer.NewClient();
+        }
+
+        [ClassCleanup]
+        public static void CleanUpTest()
+        {
+            _httpClient.Dispose();
+            _testServer.Dispose();
+        }
+
+        [TestMethod]
+        public void GetReportingById_Returns_Ok()
+        {
+            // Arrange
+            var employeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f";
+            var expectedFirstName = "John";
+            var expectedLastName = "Lennon";
+            var expectedNumReports = 0;
+
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/reporting/{employeeId}");
+            var response = getRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var reporting = response.DeserializeContent<ReportingStructure>();
+            Assert.AreEqual(expectedFirstName, reporting.Employee.FirstName);
+            Assert.AreEqual(expectedLastName, reporting.Employee.LastName);
+            Assert.AreEqual(expectedNumReports, reporting.NumberOfReports);
+        }
+    }
+}
---
+++ CodeChallenge/Controllers/ReportingController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Xml.Schema;
+using CodeChallenge.Models;
+using CodeChallenge.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace CodeChallenge.Controllers
+{
+    [ApiController]
+    [Route("api/reporting")]
+    public class ReportingController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IEmployeeService _employeeService;
+
+        public ReportingController(ILogger<ReportingController> logger, IEmployeeService employeeService)
+        {
+            _logger = logger;
+            _employeeService = employeeService;
+        }
+
+        [HttpGet("{id}", Name = "getReportingById")]
+        public IActionResult GetReportingById(String id)
+        {
+            var reporting = new ReportingStructure();
+
+            _logger.LogDebug($"Received reporting structure get request for '{id}'");
+
+            var employee = _employeeService.GetById(id);
+
+            if (employee == null)
+                return NotFound();
+
+            reporting.Employee = employee;
+            reporting.NumberOfReports = NumberOfReports(employee);
+
+            return Ok(reporting);
+        }
+
+        // Recursively traverse the tree of the reporting structure to count the nodes.
+        public int NumberOfReports(Employee employee)
+        {
+            if (employee.DirectReports == null)
+                return 0;
+
+            var total = employee.DirectReports.Count;
+
+            foreach (Employee report in employee.DirectReports)
+            {
+                total += NumberOfReports(report);
+            }
+
+            return total;
+        }
+    }
+}
